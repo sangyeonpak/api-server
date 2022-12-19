cc7dd57acee8cf3aed18a56e16728f8a3b039a41
Sangyeon Pak <sangyeonpak1994@gmail.com>
2022-12-19 15:45:32 -0600

    found a creative way to identify which button is being pressed; the biggest stepping stone for my patch request

--- client/app.js
+++ client/app.js
@@ -1,188 +1,180 @@
-let $inventory = $(".inventory");
-let $inventory2 = $(".inventory2");
-let $mainContainer = $(".mainContainer");
-let $addTableButton = $(".addTableButton");
-$inventory2.hide();
-let $tablesList = $(".tablesList");
-let $searchResults = $(".searchResults");
-let $mainItemsList = $('.mainItemsList').toggle();
-let $addItemButton = $('#addItemButton');
-let $addItemModal = $('#addItemModal');
+const $roomsList = $(".roomsList");
+const $mainContainer = $(".mainContainer");
+const $addRoomButton = $(".addRoomButton");
+const $mainItemsList = $('.mainItemsList');
+$mainItemsList.hide();
+
+const $modalBody = $('#modalBody');
+const $addItemModal = $('#addItemModal');
 $addItemModal.hide();
-$mainContainer.append($mainItemsList);
-let $modalBody = $('#modalBody');
-let $toggleModal = $('.toggleModal');
 
+//============================================ do GET request then append items to the main inventory div =============================================
 $.get("/api/main").then((data) => {
-
-  for (let key in data[0]){
+  for (let key in data[0]){ // appends "rooms" (my columns) to the main screen (dynamic; pulls columns from DB)
     if (key !="name" && key != "total"){
-      let $tables = $(`<div class="tables" id="${key}Total"><div>`);
-      let $tablesForModal = $(`
+      const $rooms = $(`<div class="rooms" id="${key}Total"></div>`);
+      const $roomsForModal = $(`
       <div>
         # in ${key}:
         <input type="number" name="${key}" id="${key}Count" min="0" class="addItemInputs">
       </div>`);
-      $tablesList.append($tables);
-      $modalBody.append($tablesForModal);
-      $tables.text(`${key} ⋮`);
+      $(".roomsList").append($rooms);
+      $('#modalBody').append($roomsForModal);
+      $rooms.text(`${key} ⋮`);
     }
   }
-
-  for(items of data){
-
-    let $itemsInMainList = $('<div class="item"></div>');
+  for(items of data){ // appends items items list (dynamic; pulls items from DB)
+    const $itemsInMainList = $(`<div class="item" id="${items.name}Main" name="${items.name}"></div>`);
     $itemsInMainList.html(`${items.name} total: ${items.total} <br>`);
     for (let key in items){
       if (key !="name" && key != "total"){
-        // console.log(key);
-        // console.log($itemsInMainList.html());
-        $itemsInMainList.html(($itemsInMainList.html()).concat(`&nbsp&nbsp&nbsp&nbsp&nbsp${key}: ${items[key]} <br>`));
+        $itemsInMainList.html(($itemsInMainList.html()).concat(`<div id=${items.name}>&nbsp&nbsp&nbsp&nbsp&nbsp${key}: ${items[key]} <button class="btn patchButton" name="${items.name}" value="${key}">+</button><button class="btn patchButton" name="${items.name}" value="${key}">-</button><br></div>`));
       }
     }
-    $mainItemsList.append($itemsInMainList);
+    $(".mainItemsList").append($itemsInMainList);
   }
-
+  whichPatchButton($(`.patchButton`));
 });
 
+//============================================== inventory and inventory2 buttons have slightly different functionality ===============================
+const $inventory = $(".inventory");
+const $inventory2 = $(".inventory2");
+$inventory2.hide();
+
+const $searchArea = $(".searchArea");
+const $searchBar = $("#searchBar");
+const $searchResults = $(".searchResults");
+const $itemAlreadyExists = $('.itemAlreadyExists');
 
 $inventory.click(() => {
   $inventory2.show();
-  $('.tablesList').hide(100);
-  $('.mainItemsList').show(100);
+  $roomsList.hide(100);
+  $mainItemsList.show(100);
   $searchResults.empty();
   $inventory.hide();
-  $addTableButton.detach();
+  $addRoomButton.detach();
   $itemAlreadyExists.hide();
 })
 
-$inventory2.click(() => {
+function showMainMenu(){
   $inventory.show();
-  $('.tablesList').show(100);
-  $('.mainItemsList').hide(100);
+  $roomsList.show(100);
+  $mainItemsList.hide(100);
   $searchResults.empty();
   $inventory2.hide();
-  $mainContainer.append($addTableButton);
+  $mainContainer.append($addRoomButton);
   $itemAlreadyExists.hide();
+}
+
+$inventory2.click(() => {
+  showMainMenu();
 })
 
-let $searchArea = $(".searchArea");
-let $searchBar = $("#searchBar");
+//========================================== do GET request of search query, then append items to search results div ==================================
 $searchArea.submit((event) => {
   event.preventDefault();
-  $('.tablesList').hide(100);
-  $('.mainItemsList').hide(100);
   $searchResults.empty();
+  $roomsList.hide(100);
+  $mainItemsList.hide(100);
   $inventory.hide();
   $inventory2.show();
-  $addTableButton.detach();
-
-  if ($searchBar.val() === '' || $searchBar.val() === null) {
-    $searchResults.html("<h5>Type something to search!</h5>");
-  }
-
+  $itemAlreadyExists.hide();
+  $addRoomButton.detach();
+  if ($searchBar.val() === '' || $searchBar.val() === null) $searchResults.html("<h5>Type something to search!</h5>");
   $.get(`/api/items/${$searchBar.val()}`).then((data) => {
-    console.log(data.length);
-    console.log(data);
-    if(data.length === 0){
-      $searchResults.append("<h5>Nothing found!</h5>")
-    }
+    if(data.length === 0) $searchResults.append("<h5>Nothing found!</h5>");
     for(items of data){
-      let $resultingItem = $('<div class="searchedItem"></div>');
+      const $resultingItem = $(`<div class="searchedItem" id=${items.name}Searched name="${items.name}"></div>`);
       $resultingItem.html(`${items.name} total: ${items.total} <br>`);
       for (let key in items){
         if (key !="name" && key != "total"){
-          // console.log(key);
-          // console.log($resultingItem.html());
-          $resultingItem.html(($resultingItem.html()).concat(`&nbsp&nbsp&nbsp&nbsp&nbsp${key}: ${items[key]} <br>`));
+          $resultingItem.html(($resultingItem.html()).concat(`&nbsp&nbsp&nbsp&nbsp&nbsp${key}: ${items[key]} <button class="btn patchButtonSearch" name="${items.name}" value="${key}">+</button><button class="btn patchButtonSearch" name="${items.name}" value="${key}">-</button><br>`)); //&nbsp ftw
         }
       }
       $searchResults.hide().show(100);
       $searchResults.append($resultingItem);
     }
+    whichPatchButton($(`.patchButtonSearch`));
   })
 })
 
-let $itemAlreadyExists = $('.itemAlreadyExists');
-
+//=================================== on click, trigger modal, do POST request, then append items to main inventory div ===============================
+const $addItemButton = $('#addItemButton');
+const $itemAddedAlert = $('#itemAddedAlert');
+$itemAddedAlert.hide();
 $addItemButton.click(() => {
-  let $modalBackdrop = $('.modal-backdrop');
-  let $addItemInputs = $('.addItemInputs');
-  let $addItemName = $('#addItemName');
-
-
-  /* what am i trying to do
-  1) since my input fields are dynamically generated, i have to find a way to get the ids of each input fields
-
-  2) when they click "save change" button, gotta do post request for all count boxes
-  2.1) if val is null, 0
-  3) append results to main inventory menu
-
-  */
-
-  // let postRequestBody = {};
-  // for (inputFields of $addItemInputs){
-  //   let idAlsoKey =
-  //   postRequestBody.inputFields = 0;
-  //   console.log(inputFields);
-  // }
-
-  // for ($addItemInputs.length)
+  const $addItemInputs = $('.addItemInputs');
+  const $addItemName = $('#addItemName');
+  const $form = $('#addItemForm');
   $addItemName.val(null);
   $addItemInputs.val(null);
-
-  // $toggleModal.click(() => {
-
-    // });
-    const $form = $('#addItemForm');
-    $form.submit((event) => {
-      event.preventDefault();
-      const data = new FormData(event.target);
-      const requestBody = {};
-      for (var [key, value] of data.entries()) {
-        // console.log(key, value);
-        requestBody[key] = value;
+  $form.submit((event) => { // unfortunately gotta leave $form inside to reinitialize event listener
+    event.preventDefault();
+    let data = new FormData(event.target);
+    let requestBody = {};
+    for (var [key, value] of data.entries()) {
+      requestBody[key] = value;
+    }
+    const $itemsInMainList = $('<div class="item"></div>');
+    fetch(`/api/items`, {
+      headers: {
+        "Content-Type": "application/json",
+        'Accept': 'application/json'
+      },
+      method: "POST",
+      body: JSON.stringify(requestBody)
+    }).then((response, error) => {
+      if (response.status !== 500) return response.json(); //! case 1
+      else { //! case 2
+        $itemAlreadyExists.empty();
+        $itemAlreadyExists.append(`Item already exists: ${requestBody.name}`)
+        $itemAlreadyExists.show();
+        $roomsList.hide(100);
+        $mainItemsList.hide(100);
+        $addRoomButton.detach();
+        $inventory.hide();
+        $inventory2.show();
       }
-
-
-    let $itemsInMainList = $('<div class="item"></div>');
-      fetch(`/api/items`, {
-        headers: {
-          "Content-Type": "application/json",
-          'Accept': 'application/json'
-        },
-        method: "POST",
-        body: JSON.stringify(requestBody)
-      }).then((response) => {
-        if (response.status !== 500) return response.json();
-
-          else {
-          $itemAlreadyExists.empty();
-          $itemAlreadyExists.append(`Item already exists: ${requestBody.name}`)
-          $itemAlreadyExists.show();
-          $('.tablesList').hide(100);
-          $('.mainItemsList').hide(100);
-          $addTableButton.detach();
-          $inventory.hide();
-          $inventory2.show();
-          }
-
-      }).then((response) => {
-
-        console.log(response);
-        let $itemsInMainList = $('<div class="item"></div>');
-        $itemsInMainList.html(`${requestBody.name} total: ${Number(requestBody.kitchen) + Number(requestBody.bathroom)} <br>`);
-        for (let key in requestBody){
-          if (key !="name" && key != "total"){
-           console.log(key);
-          // console.log($itemsInMainList.html());
-          $itemsInMainList.html(($itemsInMainList.html()).concat(`&nbsp&nbsp&nbsp&nbsp&nbsp${key}: ${items[key]} <br>`));
-          }
+    }).then((response) => {
+      if (response === undefined) return; //! case 2, continue below for case 1
+      showMainMenu();
+      $itemAddedAlert.show();
+      setTimeout(() => $itemAddedAlert.hide(100), "1000");
+      const $itemsInMainList = $('<div class="item"></div>');
+      let total = 0;
+      for (let key in requestBody){
+        if (key !="name" && key != "total") {
+          total += Number(requestBody[key]);
         }
-        $mainItemsList.append($itemsInMainList);
-
-      });
-    $form.unbind('submit');
+      }
+      $itemsInMainList.html(`${requestBody.name} total: ${total} <br>`); // gotta find a way later
+      for (let key in requestBody){
+        if (key !="name" && key != "total"){
+          $itemsInMainList.html(($itemsInMainList.html()).concat(`&nbsp&nbsp&nbsp&nbsp&nbsp${key}: ${requestBody[key]} <br>`));
+        }
+      }
+      $(".mainItemsList").append($itemsInMainList);
     });
-
+  $form.unbind('submit'); // removes event listener at the end to not have duplicate input later
   })
+})
+
+function patchRequest(){
+  fetch(`/api/items`, {
+    headers: {
+      "Content-Type": "application/json",
+      'Accept': 'application/json'
+    },
+    method: "PATCH",
+    body: JSON.stringify(requestBody)
+  })
+}
+
+function whichPatchButton (patchButton) {
+  $(patchButton).click((event)=>{
+    console.log(event.target.name);
+    console.log(event.target.value);
+    console.log(event.target.textContent);
+  })
+}
+whichPatchButton($(`.patchButton`));
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
+//?sudo service postgresql start
 import postgres from "postgres";
 import express from "express";
 import morgan from "morgan";
 import dotenv from "dotenv";
-//sudo service postgresql start
 dotenv.config();
 
-// const sql = postgres(process.env.DATABASE_URL);
+// const sql = postgres(process.env.DATABASE_URL) = production db;
 const sql = postgres({database: "inventory", username: process.env.DB_USERNAME, password: process.env.DB_PASSWORD});
 const app = express();
 const port = 3001;
@@ -14,40 +14,28 @@ app.use(express.json());
 app.use(express.static("./client"));
 app.use(morgan('tiny'));
 
-
 app.get("/api/main", (req, res, next) => {
   sql`SELECT * FROM items`.then((result) => {
     res.json(result);
   }).catch(next);
 })
 
-
-
 app.get("/api/items/:item", (req, res, next) => {
-  console.log(req);
   const item = req.params.item;
-  // console.log(isNaN(Number(item)));
-  // console.log(Number(item) === NaN); found out this doesn't work
   if (item === '' || item === null) res.status(400).send("Type something!");
-  sql`SELECT * FROM items WHERE name ILIKE ${'%' + item + '%'}`.then((result) => { // funny because it took a while for me to figure it out
-    console.log(item);
+  sql`SELECT * FROM items WHERE name ILIKE ${'%' + item + '%'}`.then((result) => {
     res.status(200).json(result);
-    // if (result.length > 0) res.status(200).json(result);
-    // else (res.status(404).json([{}]));
   }).catch(next);
 })
 
-
-
 app.post("/api/items", (req, res, next) => {
-  // const { name } = req.body;
   const {name,kitchen,bathroom} = req.body;
   if (name !== ''){
     sql`INSERT INTO items (name,kitchen,bathroom) VALUES (${name},${kitchen},${bathroom}) RETURNING *`.then((result) => {
-      console.log(result);
       res.status(201).json(result[0]);
     }).catch(next)
-    // console.log(req.body);
+    // experimenting with dynamic columns... psql doesn't like that
+    //console.log(req.body);
     // let variableColumns = [];
     // let variableValuesArray = [];
     // for (let keys in req.body){
@@ -71,11 +59,9 @@ app.post("/api/items", (req, res, next) => {
     // sql`INSERT INTO items (${variableColumns}) VALUES (${name},${variableValues}) RETURNING *`.then((result) => {
     //   console.log(result);
     //   res.status(201).json(result[0]);
-    // }).catch(next)
+    // }).catch(next)*/
   }
 })
-
-
 
 app.patch("/api/items/:item", (req, res, next) => {
   const item = req.params.item;
@@ -85,8 +71,6 @@ app.patch("/api/items/:item", (req, res, next) => {
   }).catch(next)
 })
 
-
-
 app.delete("/api/items/:item", (req, res, next) => {
   const item = req.params.item;
   sql`DELETE FROM items WHERE item ILIKE ${item}`.then((result) => {
@@ -94,20 +78,14 @@ app.delete("/api/items/:item", (req, res, next) => {
   }).catch(next)
 })
 
-
-
 app.use((err, req, res, next) => {
   console.log(err);
   res.status(500).json(req.body)
 })
 
-
-
 app.all('*', (req, res) => {
   res.status(404).send("Not found!");
 })
-
-
 
 app.listen(port, () => {
   console.log("Listening on port ", port);
